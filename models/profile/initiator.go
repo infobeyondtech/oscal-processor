038c7aa980b606232747474c9acef3abe038e1e7
Tom Tang <x.long@infobeyondtech.com>
2020-04-22 17:39:45 -0400

    k

---
+++ models/profile/initiator.go
@@ -0,0 +1,11 @@
+// Provides the functionality of creating a profile
+package profile
+
+import (
+	sdk_profile "github.com/docker/oscalkit/types/oscal/profile"
+)
+
+// Given a set of controls, a set of catalogs, and a baseline,
+// generates a profile
+func create(ctrls []string, b string, ctlgs []string) *sdk_profile.Profile {
+}
