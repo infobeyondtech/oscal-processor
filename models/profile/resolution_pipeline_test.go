c5248f331c3b6fb8518e91b3e40982023dd0b2e9
Tom Tang <x.long@infobeyondtech.com>
2020-04-23 11:29:17 -0400

    Fix assertion

--- models/profile/resolution_pipeline_test.go
+++ models/profile/resolution_pipeline_test.go
@@ -3,12 +3,12 @@ package profile
 import "testing"
 
 // Test
-func TestRun(t *testing.T) {
+func TestResolveProfile(t *testing.T) {
 	rules := "/home/tom/oscal_workspace/OSCAL/src/utils/util/resolver-pipeline/oscal-profile-resolve-select.xsl"
 	jarPath := "/home/tom/.nanshiie_baker/jars/saxon-he-10.0.jar"
 	dir := "/home/tom/oscal_processing_space"
 	input := "/home/tom/oscal_workspace/OSCAL/src/utils/util/resolver-pipeline/testing/pathological-profile.xml"
 	output := dir + "/output/selected.xml"
 	e := ResolveProfile(jarPath, rules, input, output)
-	t.Error(e == nil)
+	t.Error(e != nil)
 }
