8710f82703631f1a5ed915eaa6f7f0750aeb812c
David Perry <davidmitchelperry@gmail.com>
2021-04-05 14:11:31 -0700

    Added Control model

---
+++ models/control/initiator.go
@@ -0,0 +1,56 @@
+package control
+
+import (
+    "encoding/json"
+    "database/sql"
+    _ "github.com/go-sql-driver/mysql"
+    "fmt"
+)
+
+type Param struct {
+    // Unique identifier of the containing object
+    Id string `xml:"id,attr,omitempty" json:"id,omitempty"`
+    // A short name for the parameter.
+    Label string `xml:"label,omitempty" json:"label,omitempty"`
+}
+
+type Part struct {
+    // Unique identifier of the containing object
+    Id string `xml:"id,attr,omitempty" json:"id,omitempty"`
+    // Identifying the purpose and intended use of the property, part or other object.
+    Name string `xml:"name,attr,omitempty" json:"name,omitempty"`
+    Prose string `xml:"prose,omitempty" json:"prose,omitempty"`
+    // A partition or component of a control or part
+    Parts []Part `xml:"part,omitempty" json:"parts,omitempty"`
+}
+
+type Control struct {
+    // Unique identifier of the containing object
+    Id string `xml:"id,attr,omitempty" json:"id,omitempty"`
+    // Parameters provide a mechanism for the dynamic assignment of value(s) in a control.
+    Parameters []Param `xml:"param,omitempty" json:"parameters,omitempty"`
+    // A partition or component of a control or part
+    Parts []Part `xml:"part,omitempty" json:"parts,omitempty"`
+}
+
+func GetControl(ctrlId string) (Control) {
+    db, err := sql.Open("mysql", "root_master:root@(216.84.167.166:3306)/cube");
+    if err != nil {
+        panic(err.Error())
+    }
+    var queryResult string;
+    query, err := db.Query(`call GetControlTree('` + ctrlId + `', @result)`)
+    if err != nil {
+        panic(err.Error())
+    }
+    query.Next()
+    query.Scan(&queryResult)
+    fmt.Println(queryResult)
+    result := Control{}
+    marshalError := json.Unmarshal([]byte(queryResult), &result)
+    if marshalError != nil {
+        panic(marshalError)
+    }
+    fmt.Println(result);
+    return result;
+}
