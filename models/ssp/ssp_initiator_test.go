795dc3d167e940e6ab27f39c5835c6a63cf1ee6e
Hao Peng <haopeng1993@hotmail.com>
2021-04-14 11:25:38 -0400

    test

--- models/ssp/ssp_initiator_test.go
+++ models/ssp/ssp_initiator_test.go
@@ -3,6 +3,7 @@ package ssp
 import (
 	"testing"
 	"fmt"
+	"github.com/stretchr/testify/assert"
 	sdk_ssp "github.com/docker/oscalkit/types/oscal/system_security_plan"
 	//request_models "github.com/infobeyondtech/oscal-processor/models/requests"
 	information "github.com/infobeyondtech/oscal-processor/models/information"
@@ -16,21 +17,26 @@ func TestCreateSSP(t *testing.T) {
 }
 
 func TestAddUser(t *testing.T){
+	assert := assert.New(t)
 	ssp := &sdk_ssp.SystemSecurityPlan{}
+	roleId := "asset-owner"
+	title := "Audit Team"
 	db_user := &information.User{
 		Id: "2",
 		UUID: "ae8de94c-835d-4303-83b1-114b6a117a07",
-		RoleId: "asset-owner",
+		RoleId: roleId ,
 		Type: "internal",
-		Title: "Audit Team",
+		Title: title,
 	}
 	AddUser(ssp, *db_user)
 
-	// todo: check the field in ssp
-
+	firstUser := ssp.SystemImplementation.Users[0]
+	
 	WriteToFile(ssp)	// this function will initiate file id if not set
-
 	fmt.Printf("file name:" +ssp.Id)
 
+	assert.Equal(string(firstUser.Title), title)
+	assert.Equal(string(firstUser.RoleIds[0]), roleId)
 }
+
 
