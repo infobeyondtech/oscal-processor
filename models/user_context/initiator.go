d7ef67faab89e3ed6b1dd64a555a85292c8fa577
Hao Peng <haopeng1993@hotmail.com>
2021-08-17 18:11:34 -0400

    set user ssp

--- models/user_context/initiator.go
+++ models/user_context/initiator.go
@@ -18,6 +18,16 @@ type NullableUserContext struct {
     ProfileFileId sql.NullString `xml:"profilefileid,omitempty" json:"profilefileid,omitempty"`
 }
 
+type UserSsp struct{
+    ProjectId string `xml:"projectid,attr,omitempty" json:"projectid,omitempty"`
+    SspFileId string `xml:"sspFileId,omitempty" json:"sspFileId,omitempty"`
+}
+
+type NullableUserSsp struct{
+    ProjectId sql.NullString `xml:"projectid,attr,omitempty" json:"projectid,omitempty"`
+    SspFileId sql.NullString `xml:"sspFileId,omitempty" json:"sspFileId,omitempty"`
+}
+
 // Given a set of controls, a set of catalogs, and a baseline,
 // generate a unique ID, which can be used for the following operations.
 func SetUserContext(projectId string, profileFileId string) UserContext {
@@ -45,7 +55,6 @@ func SetUserContext(projectId string, profileFileId string) UserContext {
         result := UserContext{projectId, profileFileId}
         return result
 }
-
 
 func GetProfileFileId(projectId string) UserContext {
     var result UserContext
@@ -80,3 +89,119 @@ func GetProfileFileId(projectId string) UserContext {
 
     return result
 }
+
+func UpdateUserContext(projectId string, profileFileId string) UserContext{
+    db, err := sql.Open("mysql", context.DBSource)
+    if err != nil {
+        panic(err.Error())
+    }
+    defer db.Close()
+
+    query := `UPDATE user_context Set profileFileId = "`
+    query += profileFileId
+    query += `" WHERE projectId = "`
+    query += projectId
+    query += `");`
+    _, err = db.Exec(query)
+    if err != nil {
+        fmt.Println("Caused by: " + query)
+        panic(err.Error())
+    }
+
+    result := UserContext{projectId, profileFileId}
+    return result
+}
+
+
+
+
+func AddUserSsp(projectId string, sspFileId string) UserSsp {
+    db, err := sql.Open("mysql", context.DBSource)
+    if err != nil {
+        panic(err.Error())
+    }
+    defer db.Close()
+    query := `INSERT INTO user_ssp(projectId, sspFileId) Values("`
+    query += projectId
+    query += `", "`
+    query += sspFileId
+    query += `")`
+    _, err = db.Exec(query)
+    if err != nil {
+        fmt.Println("Caused by: " + query)
+        panic(err.Error())
+    }
+
+    result := UserSsp{projectId, sspFileId}
+    return result
+}
+
+func GetSspFileId(projectId string) UserSsp {
+    var result UserSsp
+    var nullableResult NullableUserSsp
+    
+    db, err := sql.Open("mysql", context.DBSource)
+    if err != nil {
+        panic(err.Error())
+    }
+    defer db.Close()
+    query := `SELECT projectId, sspFileId From user_ssp WHERE projectId = "`
+    query += projectId
+    query += `";`
+    err = db.QueryRow(query).
+        Scan(&nullableResult.ProjectId, &nullableResult.SspFileId)
+    if err != nil {
+        panic(err.Error())
+    }
+
+    if nullableResult.ProjectId.Valid {
+        result.ProjectId = nullableResult.ProjectId.String
+    } else {
+        result.ProjectId = ""
+    }
+    
+    if nullableResult.SspFileId.Valid {
+        result.SspFileId = nullableResult.SspFileId.String
+    } else {
+        result.SspFileId = ""
+    }
+
+    // if no record is found, return an empty record
+    return result
+}
+
+func UpdateUserSsp(projectId string, sspFileId string) UserSsp{
+    // update user ssp record disreguard what the current ssp is
+    db, err := sql.Open("mysql", context.DBSource)
+    if err != nil {
+        panic(err.Error())
+    }
+    defer db.Close()
+
+    query := `UPDATE user_ssp Set sspFileId = "`
+    query += sspFileId
+    query += `" WHERE projectId = "`
+    query += projectId
+    query += `");`
+    _, err = db.Exec(query)
+    if err != nil {
+        fmt.Println("Caused by: " + query)
+        panic(err.Error())
+    }
+
+    result := UserSsp{projectId, sspFileId}
+    return result
+}
+
+func SetUserSsp(projectId string, sspFileId string) UserSsp{
+    currentSsp := GetSspFileId(projectId)
+    if currentSsp.SspFileId == "" {
+        return AddUserSsp(projectId, sspFileId)
+    }else{
+        return UpdateUserSsp(projectId, sspFileId)
+    }
+}
+
+
+
+
